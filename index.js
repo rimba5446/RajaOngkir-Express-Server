e4db2bf5ca96766af078cdb52a1dc29151e2aa64
RaykujanK13 <54540238+rimba5446@users.noreply.github.com>
2022-06-29 08:36:54 +0700

    Update Index

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-// Router
+
 const apiRouter = require("./routes/api");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Definisikan Router pada path "/api"
+
 app.use("/api", apiRouter);
 
-// Home
+
 app.get("/", function (req, res) {
   res.send({
     message: "Hello API nya Bisa",
